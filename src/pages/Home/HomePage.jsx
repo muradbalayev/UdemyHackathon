27f3acdc10d08afab7522ee2d64996f60c625c0b
Murad Balayev <muradbalazade@gmail.com>
2024-09-15 01:53:26 +0400

    CreateCourse

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
+    return (
+        <div>
+            HomePage
+            <Link to={'/create-course'} className="btn px-4 py-2 bg-[#3722d3]"> Create Course</Link>
+        </div>
+    )
 }
 
 export default HomePage
