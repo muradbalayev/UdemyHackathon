ca35a713aa83aa291ea85d90d4ef9b5dd13e73b4
amin <lolotron022@gmail.com>
2024-09-15 03:53:06 +0400

    homepage Fix

--- src/pages/Users/HomePage/HomePage.jsx
+++ src/pages/Users/HomePage/HomePage.jsx
@@ -1,16 +1,30 @@
 import Nav from '../../../components/Nav'
 import { Link } from 'react-router-dom'
+import LearnFrom from './LearnFrom'
+import WhyLowe from './WhyLowe'
+import AskedQuest from './AskedQuest'
+import Footer from './Footer'
+import CreativeLearning from './CreativeLearning'
 
 const HomePage = () => {
-    return (
-        <div>
-            <Nav />
-            <div className="p-8">
-                <Link to='/create-course' className="font-bold btn px-4 py-2 bg-[#00FF84]"> Create Course</Link>
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <Nav />
+      <div className="p-8">
+        <Link to='/create-course' className="font-bold btn px-4 py-2 bg-[#00FF84]"> Create Course</Link>
+      </div>
+      <div className="container mx-auto pt-8">
+        < LearnFrom />
+        <CreativeLearning />
+        {/* <div className="text-center mt-8">
+          <h2 className="text-2xl font-bold">Explore Inspiring Online Courses</h2>
+        </div> */}
+        <WhyLowe />
+        <AskedQuest />
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
