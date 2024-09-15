d48681cbea09de099030e196515a9d886c772047
Murad Balayev <muradbalazade@gmail.com>
2024-09-15 06:07:37 +0400

    update

--- src/pages/Users/HomePage/HomePage.jsx
+++ src/pages/Users/HomePage/HomePage.jsx
@@ -10,12 +10,13 @@ const HomePage = () => {
   return (
     <div>
       <Nav />
+      
+      <div className="mx-auto pt-8 bg-[#000] text-white">
       <div className="p-8">
         <Link to='/create-course' className="font-bold btn px-4 py-2 bg-[#00FF84]"> Create Course</Link>
       </div>
-      <div className="mx-auto pt-8">
-        < LearnFrom />
         <CreativeLearning />
+        < LearnFrom />
         {/* <div className="text-center mt-8">
           <h2 className="text-2xl font-bold">Explore Inspiring Online Courses</h2>
         </div> */}
--- src/pages/Users/HomePage/LearnFrom/LearnFrom.jsx
+++ src/pages/Users/HomePage/LearnFrom/LearnFrom.jsx
@@ -2,12 +2,12 @@ import './LearnFrom.scss';
 
 function LearnFrom() {
   return (
-    <>
+    <div className='mt-20'>
       <h1 className="text-4xl font-bold text-center mb-8">Learn from Creative Experts</h1>
       <p className="flex justify-center text-center px-4">
         Skillshare classes are taught by industry leaders excited to share their tools, techniques, and professional journeys with you.
       </p>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-12 w-3/6 left-1/2 relative -translate-x-1/2 mt-12">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-12 relative mt-12 px-10">
         <div className="LearnFrom__img-box">
           <p className="LearnFrom__img-box-text">asdsadas</p>
           <img src="https://static.skillshare.com/assets/images/loh/marques_brownlee.webp" alt="" />
@@ -34,7 +34,7 @@ function LearnFrom() {
         </div>
       </div>
       <div className="grid md:grid-cols-2 grid-cols-1"></div>
-    </>
+    </div>
   )
 }
 
